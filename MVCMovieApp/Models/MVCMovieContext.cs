ffef822491b27fdc9fdeb334609569b4da03273e
yingying <chen.yingying.01988@gmail.com>
2018-10-31 15:52:58 +0000

    ESP and Entity

---
+++ MVCMovieApp/Models/MVCMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCMovieApp.Models
+{
+    public class MVCMovieContext : DbContext
+    {
+        public MVCMovieContext(DbContextOptions<MVCMovieContext> options) :base(options)
+        {
+        }
+
+        public DbSet<MVCMovieApp.Models.Movie> Movies { get; set; }
+    }
+}
---
+++ MVCMovieApp/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVCMovieApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        //[DisplayFormat (DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
